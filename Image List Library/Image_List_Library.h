7d8e057e4a9a114720b4ef7f1369fcac6c26711f
Bayu Wijaya Permana Putra <bayu_wpp@programmer.net>
2019-11-25 17:25:59 +0700

    Initial Commit

---
+++ Image List Library/Image_List_Library.h
@@ -0,0 +1,19 @@
+//
+//  Image_List_Library.h
+//  Image List Library
+//
+//  Created by PayTren on 11/25/19.
+//  Copyright © 2019 PayTren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Image_List_Library.
+FOUNDATION_EXPORT double Image_List_LibraryVersionNumber;
+
+//! Project version string for Image_List_Library.
+FOUNDATION_EXPORT const unsigned char Image_List_LibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Image_List_Library/PublicHeader.h>
+
+
